d4580eb7c5ffc29e9f0a5d9cad870c67541aa0cd
Lukow4 <luke.kowieski@gmail.com>
2022-12-10 13:30:38 +0100

    netlify changes

---
+++ dist/contact.71273288.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName("toggle_button")[0];
+const navBarLinks = document.getElementsByClassName("navbar_links")[0];
+toggleButton.addEventListener("click", ()=>{
+    navBarLinks.classList.toggle("active");
+});
+
+//# sourceMappingURL=contact.71273288.js.map
---
+++ dist/prices.e4bcb6e9.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName("toggle_button")[0];
+const navBarLinks = document.getElementsByClassName("navbar_links")[0];
+toggleButton.addEventListener("click", ()=>{
+    navBarLinks.classList.toggle("active");
+});
+
+//# sourceMappingURL=prices.e4bcb6e9.js.map
