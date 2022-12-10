0e47ca926f553a414090b9cd3fc95e64e6c4f201
Lukow4 <luke.kowieski@gmail.com>
2022-12-10 13:16:46 +0100

    clean repo

--- dist/dacquoise.51f1dda4.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementsByClassName("toggle_button")[0];
-const navBarLinks = document.getElementsByClassName("navbar_links")[0];
-toggleButton.addEventListener("click", ()=>{
-    navBarLinks.classList.toggle("active");
-});
-
-//# sourceMappingURL=dacquoise.51f1dda4.js.map
--- dist/prices.1e148738.js
+++
@@ -1,2 +0,0 @@
-const toggleButton=document.getElementsByClassName("toggle_button")[0],navBarLinks=document.getElementsByClassName("navbar_links")[0];toggleButton.addEventListener("click",(()=>{navBarLinks.classList.toggle("active")}));
-//# sourceMappingURL=prices.1e148738.js.map
