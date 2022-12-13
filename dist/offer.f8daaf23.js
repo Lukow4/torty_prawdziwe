188c202a86fc789633b30ee8ae9fd1b7bfd61a46
Lukow4 <luke.kowieski@gmail.com>
2022-12-13 16:20:27 +0100

    change cal style

--- dist/offer.f8daaf23.js
+++ dist/offer.f8daaf23.js
@@ -8,11 +8,9 @@ function computeLoan() {
     const interest_rate = document.querySelector("#interest_rate").value;
     const months = document.querySelector("#months").value;
     const interest = amount * interest_rate * 0.1 * months;
-    let payment = (interest + months * 10).toFixed(); //calculate monthly payment
-    //regedit to add a comma after every three digits
+    let payment = (interest + months * 10).toFixed();
     payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    //"innerHTML" will help you to see the information in the webpage
-    document.querySelector("#payment").innerHTML = `Monthly Payment = ${payment}`;
+    document.querySelector("#payment").innerHTML = `Orientacyjna cena = ${payment}`;
 }
 
 //# sourceMappingURL=offer.f8daaf23.js.map
